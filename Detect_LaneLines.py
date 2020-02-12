4c439b74f0c1255b7bbf5b2a0a6a09e461fd8933
Yasmin Motawea <ymotawea@gmail.com>
2020-02-12 17:20:04 +0200

    Create Detect_LaneLines.py

---
+++ Detect_LaneLines.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    canny = cv2.Canny(blur, 50, 150)
+    return canny
+
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons= np.array([
+    [(200, height), (1100, height), (550, 250)]
+    ])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+image = cv2.imread('test_image.jpg')
+canny = canny (image)
+cropped_image = region_of_interest(canny)
+lane_image = np.copy(image)
+
+cv2.imshow("result", cropped_image)
+cv2.waitKey(0)
